cac6d9d698915ba1cc628c376eb7c5ae06525524
hasmokan <1021056159@qq.com>
2025-01-15 23:49:07 +0800

    feat: lib

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use ::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
